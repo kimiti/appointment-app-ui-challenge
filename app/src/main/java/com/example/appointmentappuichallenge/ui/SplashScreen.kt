3a63e38d2d9c18a95288278ba7d68a195ecaeafb
kimiti <kimitikevin@gmail.com>
2025-03-29 08:00:00 +0300

    add splash screen

---
+++ app/src/main/java/com/example/appointmentappuichallenge/ui/SplashScreen.kt
@@ -0,0 +1,10 @@
+package com.example.appointmentappuichallenge.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun SplashScreen(){
+    
+}
