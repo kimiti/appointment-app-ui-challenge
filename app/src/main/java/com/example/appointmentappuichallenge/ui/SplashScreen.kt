8a25e1352ce3bf4cd350233df1f5a64550325d8b
kimiti <kimitikevin@gmail.com>
2025-03-31 08:00:00 +0300

    adds splash screen

--- app/src/main/java/com/example/appointmentappuichallenge/ui/SplashScreen.kt
+++ app/src/main/java/com/example/appointmentappuichallenge/ui/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.appointmentappuichallenge.R
 
 @Preview
@@ -34,7 +36,10 @@ fun SplashScreen(onStartClick: () -> Unit = {}) {
         )
         Image(
             painter = painterResource(R.drawable.women),
-            contentDescription = null
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 325.dp)
         )
     }
 }
