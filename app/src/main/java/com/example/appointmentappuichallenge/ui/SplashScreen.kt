b2a44f298793338fcdee7c5348e58ca72a635223
kimiti <kimitikevin@gmail.com>
2025-04-01 08:00:00 +0300

    splash screen

--- app/src/main/java/com/example/appointmentappuichallenge/ui/SplashScreen.kt
+++ app/src/main/java/com/example/appointmentappuichallenge/ui/SplashScreen.kt
@@ -3,20 +3,28 @@ package com.example.appointmentappuichallenge.ui.theme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.appointmentappuichallenge.R
 
 @Preview
@@ -51,6 +59,36 @@ fun SplashScreen(onStartClick: () -> Unit = {}) {
                 color = Color.White,
                 shape = RoundedCornerShape(topStart = 50.dp, topEnd = 50.dp)
             )
-        ) { }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Spacer(modifier = Modifier.height(32.dp))
+
+                Text(
+                    text = stringResource(R.string.title1),
+                    color = Color.Black,
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 44.sp
+                )
+
+                Text(
+                    text = stringResource(R.string.title2),
+                    color = colorResource(R.color.green),
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    textAlign = TextAlign.Center,
+                    lineHeight = 44.sp,
+                    modifier = Modifier
+                        .padding(top = 12.dp)
+                )
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
     }
 }
