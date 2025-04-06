96945e2ebf4c44450ae7c1d7dcb087681f66cc46
kimiti <kimitikevin@gmail.com>
2025-04-06 08:00:00 +0300

    adds MainScreen

--- app/src/main/java/com/example/appointmentappuichallenge/ui/MainScreen.kt
+++ app/src/main/java/com/example/appointmentappuichallenge/ui/MainScreen.kt
@@ -1,28 +1,52 @@
 package com.example.appointmentappuichallenge.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview
 @Composable
-fun MainScreenContent(){
+fun MainScreenContent() {
     Scaffold() { paddingValues ->
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(paddingValues)
-            .padding(horizontal = 16.dp)
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp)
         ) {
-            
+            item {
+                Spacer(modifier = Modifier.height(36.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(modifier = Modifier.weight(1f)) {
+                        Text(text = "Hi, John Doe",
+                            color = Color.Black,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(text = "What do you feel!")
+                    }
+                }
+            }
         }
     }
 }
