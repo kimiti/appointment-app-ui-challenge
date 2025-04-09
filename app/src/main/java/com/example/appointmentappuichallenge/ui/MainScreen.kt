790932a3157644aefdce8df7345cf12d6b64ab08
kimiti <kimitikevin@gmail.com>
2025-04-09 08:20:00 +0300

    bottom nav

--- app/src/main/java/com/example/appointmentappuichallenge/ui/MainScreen.kt
+++ app/src/main/java/com/example/appointmentappuichallenge/ui/MainScreen.kt
@@ -2,6 +2,8 @@ package com.example.appointmentappuichallenge.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -23,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -37,6 +41,10 @@ import com.example.appointmentappuichallenge.R
 @Composable
 fun MainScreenContent() {
     var searchText by remember { mutableStateOf("") }
+    var selectedBottomItem by remember { mutableStateOf(0) }
+
+
+
     Scaffold() { paddingValues ->
         LazyColumn(
             modifier = Modifier
@@ -111,4 +119,34 @@ fun MainScreenContent() {
             }
         }
     }
+}
+
+
+@Composable
+fun BottomMenuItem(
+    iconId: Int,
+    title: String,
+    isSelected: Boolean,
+    onClick: () -> Unit,
+) {
+    val backgroundColor = if (isSelected) colorResource(R.color.lightGreen) else Color.Transparent
+    val content = if (isSelected) colorResource(R.color.green) else Color.Gray
+
+
+    Row(
+        modifier = Modifier
+            .clip(RoundedCornerShape(20))
+            .background(backgroundColor)
+            .clickable { onClick() }
+            .padding(horizontal = 12.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            painter = painterResource(id = iconId),
+            contentDescription = null,
+            tint = content
+        )
+    }
+
 }
