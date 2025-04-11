fd59c740d364ca3aa7a59c2f1c9a3cb26206c425
kimiti <kimitikevin@gmail.com>
2025-04-11 08:50:00 +0300

    bottom nav

--- app/src/main/java/com/example/appointmentappuichallenge/MainActivity.kt
+++ app/src/main/java/com/example/appointmentappuichallenge/MainActivity.kt
@@ -5,13 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.appointmentappuichallenge.ui.MainScreen
 import com.example.appointmentappuichallenge.ui.theme.AppointmentAppUIChallengeTheme
+import com.example.appointmentappuichallenge.ui.theme.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +21,16 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppointmentAppUIChallengeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                  
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "splash") {
+                        composable("splash"){
+                            SplashScreen()
+                        }
+                        composable("main") {
+                            MainScreen()
+                        }
+                    }
                 }
             }
         }
