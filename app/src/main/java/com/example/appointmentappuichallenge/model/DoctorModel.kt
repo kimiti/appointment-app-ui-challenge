2a5e13712e8a972cf50edac4cde0c2da9b1f79c4
kimiti <kimitikevin@gmail.com>
2025-04-14 08:00:00 +0300

    doctor model

---
+++ app/src/main/java/com/example/appointmentappuichallenge/model/DoctorModel.kt
@@ -0,0 +1,19 @@
+package com.example.appointmentappuichallenge.model
+
+data class doctorModel(
+    val Address: String = "",
+    val Biography: String = "",
+    val id: Int = 0,
+    val Name: String = "",
+    val Picture: String = "",
+    val Special: String = "",
+    val Expriense: String = "",
+    val Cost: String = "",
+    val Date: String = "",
+    val Time: String = "",
+    val Location: String = "",
+    val Mobile: String = "",
+    val Patients: String = "",
+    val Rating: String = "",
+    val Site: String = "",
+)
