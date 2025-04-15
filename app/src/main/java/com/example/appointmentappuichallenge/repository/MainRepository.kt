0e78a421c8db9b145f3d65ac3e0111a7d47e008a
kimiti <kimitikevin@gmail.com>
2025-04-15 08:20:00 +0300

    doctor model

--- app/src/main/java/com/example/appointmentappuichallenge/repository/MainRepository.kt
+++ app/src/main/java/com/example/appointmentappuichallenge/repository/MainRepository.kt
@@ -1,2 +1,11 @@
 package com.example.appointmentappuichallenge.repository
 
+import com.example.appointmentappuichallenge.model.DoctorModel
+
+class MainRepository{
+    val doctors = mutableListOf(
+        DoctorModel(
+
+        )
+    )
+}
