adb28d7a34c5afa77afa5b74de5faa40a5eb566a
kimiti <kimitikevin@gmail.com>
2025-04-16 08:10:00 +0300

    main screen

--- app/src/main/java/com/example/appointmentappuichallenge/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/appointmentappuichallenge/viewModel/MainViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.appointmentappuichallenge.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.appointmentappuichallenge.repository.MainRepository
 
 class MainViewModel: ViewModel() {
-
+    private val repository = MainRepository()
+    fun loadDoctors() = repository.doctors
 }
